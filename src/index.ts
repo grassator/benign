73f3692d41ea0b55c2c05521dc1a14c3ec57234a
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2018-10-09 20:24:14 +0200

    Avoid recreating handler for every instance of benign

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,64 @@
-class BenignConstructor {
-  static [Symbol.hasInstance]() {
+const ProxySymbol = Symbol("Benign Proxy");
+
+const handler: ProxyHandler<{ [ProxySymbol]: any }> = {
+  getPrototypeOf(obj) {
+    return obj[ProxySymbol];
+  },
+  setPrototypeOf() {
     return true;
+  },
+  isExtensible() {
+    return true;
+  },
+  preventExtensions() {
+    return true;
+  },
+  getOwnPropertyDescriptor(obj) {
+    return {
+      configurable: true,
+      writable: true,
+      enumerable: true,
+      value: obj[ProxySymbol]
+    };
+  },
+  has() {
+    return true;
+  },
+  get(obj) {
+    return obj[ProxySymbol];
+  },
+  set() {
+    return true;
+  },
+  deleteProperty() {
+    return true;
+  },
+  defineProperty() {
+    return true;
+  },
+  /** @deprecated */
+  enumerate() {
+    return [];
+  },
+  ownKeys() {
+    // Has to return prototype
+    return ["prototype"];
+  },
+  apply(obj) {
+    return obj[ProxySymbol];
+  },
+  construct(obj) {
+    return obj[ProxySymbol];
   }
-}
+};
 
 export const benign = <T = any>(): T => {
-  const proxy: any = new Proxy(BenignConstructor, {
-    getPrototypeOf() {
-      return BenignConstructor;
-    },
-    setPrototypeOf() {
+  const base = class BenignConstructor {
+    static [Symbol.hasInstance]() {
       return true;
-    },
-    isExtensible() {
-      return true;
-    },
-    preventExtensions() {
-      return true;
-    },
-    getOwnPropertyDescriptor(obj, name) {
-      if (name === "prototype") {
-        return Reflect.getOwnPropertyDescriptor(BenignConstructor, name);
-      }
-      return {
-        configurable: true,
-        writable: true,
-        enumerable: true,
-        value: proxy
-      };
-    },
-    has() {
-      return true;
-    },
-    get() {
-      return proxy;
-    },
-    set() {
-      return true;
-    },
-    deleteProperty() {
-      return true;
-    },
-    defineProperty() {
-      return true;
-    },
-    /** @deprecated */
-    enumerate() {
-      return [];
-    },
-    ownKeys() {
-      // Has to return prototype
-      return ["prototype"];
-    },
-    apply() {
-      return proxy;
-    },
-    construct() {
-      return proxy;
     }
-  });
-  return proxy;
+    static [ProxySymbol]: any;
+  };
+  base[ProxySymbol] = new Proxy(base, handler);
+  return base[ProxySymbol] as any;
 };
